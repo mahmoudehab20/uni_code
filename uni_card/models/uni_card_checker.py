c5fe672022ad8b5344939d5b3c7d4478de4b9d3d
mahmoudehab20 <me6665342@gmail.com>
2025-08-19 00:36:32 +0300

    card checker problem solved

--- uni_card/models/uni_card_checker.py
+++ uni_card/models/uni_card_checker.py
@@ -1,8 +1,6 @@
 from odoo import models,fields,api
-import base64
-from PIL import Image
-from io import BytesIO
 from pyzbar.pyzbar import decode
+from pdf2image import convert_from_bytes
 
 class UniCardChecker(models.Model):
     _name='uni.card.checker'
@@ -16,33 +14,83 @@ class UniCardChecker(models.Model):
     image=fields.Image()
     is_valid=fields.Boolean(default=True)
 
-    # @api.depends('card')
-    # def check_card(self):
-    #     for rec in self:
-    #         if not rec.card:
-    #             rec.status=""
-    #             return True
+    @api.onchange('card')
+    def _onchange_code(self):
+        """
+        Triggered whenever the PDF file is changed.
+        Converts the first page of the PDF to an image,
+        decodes the QR code from the image,
+        then checks if a student (uni.student) exists
+        with ref = decoded value.
+        If found → is_valid = True, else False.
+        """
+        if not self.card:
+            self.is_valid = False
+            self.status=""
+            self.name=""
+            self.gender=""
+            self.academic_year=""
+            self.department=""
+            self.image=False
 
-    #         # decode QR image
-    #         print(rec.card,"card@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    #         img_data = base64.b64decode(rec.card)
-    #         img = Image.open(BytesIO(img_data))
-    #         decoded = decode(img)
-    #         if decoded:
-    #             qr_data = decoded[0].data.decode("utf-8")
-    #             print(qr_data,"qr_data@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    #             # Example: search the order by the QR code content
-    #             qr_data=qr_data.split('_')
-    #             student_id=rec.env['uni.student'].search([('ref','=',qr_data[0])])
-    #             if not student_id:
-    #                 rec.status='Not Valid!'
-    #                 rec.is_valid=False
+            return
 
-    #             if student_id.state == 'valid':
-    #                 rec.name=student_id.name
-    #                 rec.gender=student_id.gender
-    #                 rec.department=student_id.department
-    #                 rec.academic_year=student_id.academic_year
-    #                 rec.image=student_id.image
-    #                 rec.status='Valid!'
-    #                 rec.is_valid=True
+        try:
+            # Convert PDF (binary) to images
+            pages = convert_from_bytes(self.card)
+            if not pages:
+                self.is_valid = False
+                self.status=""
+                self.name=""
+                self.gender=""
+                self.academic_year=""
+                self.department=""
+                self.image=False
+
+                return
+
+            # Decode QR from the first page
+            decoded_list = decode(pages[0])
+            if not decoded_list:
+                self.is_valid = False
+                self.status=""
+                self.name=""
+                self.gender=""
+                self.academic_year=""
+                self.department=""
+                self.image=False
+
+                return
+
+            qr_value = decoded_list[0].data.decode('utf-8')
+            qr_value=qr_value.split('_')
+
+            # Search for the student
+            student = self.env['uni.student'].search([('ref', '=', qr_value[0])])
+            if student:
+                self.is_valid = True
+                self.status="IS VALID!"
+                self.name=student.name
+                self.gender=student.gender
+                self.academic_year=student.academic_year
+                self.department=student.department
+                self.image=student.image
+            if not student:
+                self.is_valid=False
+                self.status=""
+                self.name=""
+                self.gender=""
+                self.academic_year=""
+                self.department=""
+                self.image=False
+
+
+        except Exception:
+            # Any error → mark as not valid
+            self.is_valid = False
+            self.status=""
+            self.name=""
+            self.gender=""
+            self.academic_year=""
+            self.department=""
+            self.image=False
